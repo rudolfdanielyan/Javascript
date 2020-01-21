6b5cdd7821b3fcc626e40a6e53c801c2ebe959be
rudolfdanielyan <rudolf.danielyan@gmail.com>
2020-01-21 22:38:14 +0400

    double cycle

---
+++ project 2/script/index.js
@@ -0,0 +1,27 @@
+// var result = '';
+
+// for (var i = 0; i < 5; i++){
+//     for (var j = 0; j < 6; j++){        
+//         if (j === 5){
+//             result += j;
+//         } else {
+//             result += j + ', ';
+//         }
+//     }
+    
+//     console.log(result);
+//     result = '';
+// }
+
+
+// var result = '';
+// var counter = 1;
+
+// for (var i = 0; i < 5; i++){
+//     for (var j = 0; j < 5; j++){
+//         result += counter++ + ' ';
+//     }
+
+//     console.log(result);
+//     result = '';
+// }
