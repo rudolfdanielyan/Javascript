da18cf0d8525898303da11d36f69a5df91777fbc
rudolfdanielyan <rudolf.danielyan@gmail.com>
2020-01-24 22:47:08 +0400

    lesson/homevork

---
+++ lesson 4/script/index.js
@@ -0,0 +1,234 @@
+// var minNum = -Infinity;
+
+// function min(arr){
+//     for (var i = 0; i < arr.length; i++){
+//        if (minNum < arr[i]){
+//            minNum = arr[i];
+//        } 
+//     }
+
+//     return;
+// }
+
+// min([4, -40, 8]);
+// var minNum = min([4, 7, 9, -10]);
+// console.log(minNum);
+
+
+
+// var nums = [-1, 300, 12, 110, -901];
+
+// for (var i = 0; i < nums.length; i++){
+//     for (var j = 0; j < nums.length; j++){
+//         if (nums[i] < nums[j]){
+//             var container = nums[i];
+//             nums[i] = nums[j];
+//             nums[j] = container;
+//         }
+//     }
+// }
+
+// console.log(nums);
+
+
+
+// var f = function (){
+//     alert('test');
+// }
+
+// f();
+
+
+
+// f();
+
+// var f = function (){
+//     alert();
+// }
+
+
+
+// function f(){
+//     console.log(arguments[0]);
+// }
+
+// f(4, 50);
+
+
+// function f (a, b, c){
+//     console.log(f.length);
+// }
+
+// f();
+
+
+
+
+// var human = new Object();
+
+// human.name = 'Exo';
+// human.age = 30;
+
+// human.sayName = function (){
+//     alert(this.name);
+// }
+
+// human.sayName();
+
+// console.log(human);
+// console.log(typeof human);
+
+
+// var human = {
+//     name : 'Exo',
+//     age : 30,
+//     sayName : function(){
+//         alert(this.name);
+//     }
+// }
+
+// human.sayName();
+// console.log(human);
+// human.lastName = 'Exoyan';
+// human.age = 31;
+
+
+
+
+// var arr = [];
+
+// function f(){
+// }
+// console.log(f instanceof Object);
+// // console.log(arr instanceof Object);//
+
+
+
+// var obj = {
+//     age : 30
+// }
+
+// function f(){
+//     console.log(this.age);
+// }
+
+// f();
+
+// obj.sayAge = f;
+// obj.sayAge();
+
+
+
+
+// console.log(this);
+
+
+
+// var a = 10;
+// console.log(window.a);
+
+
+
+// this.a;
+// console.log(window === this);
+
+
+
+// var age = 100;
+// var obj = {
+//     age : 10,
+//     sayAge : function () {
+//         f();
+        
+//     }
+// }
+// function f (){
+//     console.log(this.age);
+    
+// }
+// obj.sayAge();
+
+
+
+
+// function Person(a,b,c) {
+//     this.firstName = a;
+//     this.lastName = b;
+//     this.age = c;
+//     this.sayFullName = function () {
+//         console.log(`My name is ${this.firstName} ${this.lastName}`);
+//     }
+// }
+// var human0 = new Person('Exo', 'Exoyan', 40);
+// human0.sayFullName();
+// console.log(human0.age);
+// human1  = new Person('Vazgen', 'Matevosyan', 50);
+// human1.sayFullName();
+// console.log(human1.age);
+
+
+
+// var result = 0;
+
+// function f() {
+//     for (var i = 0; i < result.lenght; i++){
+//         if (result > result[i]){
+//             result = result[i];
+//         }
+//     }
+// }
+
+// f(10, -30, 4);
+// console.log(result);
+
+
+
+// console.log(0b10);
+// console.log(0B10);
+// console.log(0o10);
+// console.log(0O10);
+// console.log(0x10);
+// console.log(0X10);
+
+
+// console.log(typeof true.toString());
+
+// var x = 5;
+// console.log(x.toString());
+// console.log(5..toString());
+
+
+
+//homevork
+
+getMax (5, -10, 200, 3);
+ete mek tiv grvi, alert,
+gtnel maximal arjeq@,
+ete baci tvic ayl arjeq lini , alert!
+NaN isNaN(Nan).
+Infinity === Infinity
+true
+isFinite(10);
+true
+getMax(5)//
+
+
+sarqel konstructor erb var obj = new F();
+F(); ete kanchenq arandzin aranc new-i kanchi alert!
+
+var castomMath-i vra vori vra klinen  Abs, floor, max, min, round objetner@
+
+sortavorman xndirn el grel
+
+
+
+
+
+
+
+
+
+
+
+
+
